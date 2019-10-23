80ca71388dd2867525c9267d0b1a5449f8303a0e
Ayriel Ash <aya37278@bethel.edu>
2019-10-23 08:52:02 -0500

    complete base and stretch

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return (
+      <div className="About">
+          <h2>About</h2>
+          <p>This is some info about it</p>
+      </div>
+    );
+  }
+}
+
+export default About;
